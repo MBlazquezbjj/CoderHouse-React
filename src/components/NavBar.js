3475780a97321c584b406ae540e1640f124c3012
mblazquezcoder <mma@coder.com>
2022-07-27 22:44:28 -0300

    Incluyendo NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import "./NavBar.css";
+export default function NavBar() {
+    return (
+        <header>
+            <div className="interior">
+                <nav className="navegacion">
+                    <ul>
+
+                        <li>
+                            <a href="">Iniciar Sesion</a>
+                        </li>
+                        <li>
+                            <a href="">Relojes</a>
+                        </li>
+                        <li>
+                            <a href="">Galeria</a>
+                        </li>
+                        <li>
+                            <a href="">Contacto</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+
+}
