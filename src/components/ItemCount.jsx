663e2a95daa68330cea3485f2f42eeecd054ef5a
mblazquezcoder <mma@coder.com>
2022-09-01 23:02:58 -0300

    Perfeccionando Count - Login

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { useState } from "react";
-
+import swal from "sweetalert";
 function ItemCount(props) {
 
     const [counter, setCounter] = useState(props.initial);
+
     const handleIncrement = () => {
         setCounter(counter + 1);
+        if (counter < 5) {
+            setCounter(counter + 1);
+        }
     };
-    
     const handleDecrement = () => {
         setCounter(counter - 1);
+        if (counter > 1) {
+            setCounter(counter - 1);
+        }
     };
-
-    const handleClick = ()=>{
+    const handleClick = () => {
         props.onAdd(counter)
+        if (5 > 0) {
+            swal("Hecho", "Agregado con exito", "success");
+        }
+        else {
+            swal("Lo sentimos", "No hay stock disponible por el momento", "error")
+        }
     }
 
     return (
